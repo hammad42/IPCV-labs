de98da156126f3041a90b59273f48e32df3fc0ac
hammad <hammadshamim642@gmail.com>
2024-01-19 20:42:00 +0500

    create folders

--- first_class.m
+++
@@ -1,8 +0,0 @@
-image=imread('C:\Users\yoga370\Pictures\horse.jpg');
-grey=rgb2gray(image)
-black=im2bw(image)
-subplot(1,3,1),imshow(image);
-subplot(1,3,2),imshow(grey);
-subplot(1,3,3),imshow(black);
-
-
--- noice.m
+++
@@ -1,7 +0,0 @@
-x = imread('C:\Users\hamma\Downloads\horse.jpeg');
-imshow(x)
-[r,c,s] = size(x) ;
-y = imnoise(x,'salt & pepper',0.19);
-snr = 20*log10(sum(sum(sum(x.^2)))/sum(sum(sum((y-x).^2)))) ;
-figure
-imshow(y)
